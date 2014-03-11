9881dff51fd3752badacf3d4c279755d44c31bbf
Tom Morgan <morganesque@gmail.com>
2014-03-11 16:29:16 +0000

    updated the graph code to work in the new style

--- src/js/actuate/dayBarChart.js
+++ src/js/actuate/dayBarChart.js
@@ -1,43 +1,89 @@
 "undefined" == typeof actuate && (actuate = {}),
-function(t, e, a) {
-    a.dayBarChart = function(r, i, n, s) {
-        this.graph = t(r).svgGraph(i, n), this.width = i, this.height = n, this.data = s, this.max = 0, this.spaceX = 40, this.space = 4, this.barHeight = this.height / s.length, this.type = "", this.createScales = function() {
-            this.scaleX = e.scale.linear().domain([0, e.max(this.data, function(t) {
+function(t, e, a)
+{
+    a.dayBarChart = function(r, i, n, s)
+    {
+        this.graph = t(r).svgGraph(i, n), this.width = i, this.height = n, this.data = s, this.max = 0, this.spaceX = 40, this.space = 4, this.barHeight = this.height / s.length, this.type = "", this.createScales = function()
+        {
+            this.scaleX = e.scale.linear().domain([0, e.max(this.data, function(t)
+            {
                 return t.num
             })]).range([0, this.width - this.spaceX])
-        }, this.setType = function(t) {
+        }, 
+
+        this.setType = function(t)
+        {
             this.type = t, this.price = "water" == t ? a.price.water : a.price.electric
-        }, this.plotGraph = function() {
+        }, 
+
+        this.plotGraph = function()
+        {
             var t = this.scaleX,
                 e = this.spaceX,
                 r = this.barHeight,
                 i = this.width / 2,
                 n = this.price,
                 s = this.type,
-                h = this.graph.selectAll("g").data(this.data).enter().append("a").attr("xlink:href", function(t) {
+                h = this.graph.selectAll("g").data(this.data).enter().append("a")
+                    .attr("xlink:href", function(t)
+                {
                     var e = t.date.split(" "),
                         a = new Date(parseInt(e[0]) + " " + e[1] + " " + e[2]);
                     return "/" + s + "/day/" + a.getDate() + "-" + (a.getMonth() + 1) + "-" + a.getFullYear() + "/"
-                }).attr("transform", function(t, e) {
+                })
+                    .attr("transform", function(t, e)
+                {
                     return "translate(0," + (1 + e * r) + ")"
                 });
-            h.append("rect").attr("x", this.spaceX).attr("width", this.width - this.spaceX).attr("height", this.barHeight - 2).attr("class", function(t, e) {
-                return e % 7 == 0 || e % 7 == 1 ? "bar--background weekend" : "bar--background weekday"
-            }), h.append("rect").attr("x", this.spaceX).attr("class", "lk-turq").attr("width", function(e) {
-                return t(e.num)
-            }).attr("height", this.barHeight - (2 + 2 * this.space)).attr("y", this.space), h.append("text").attr("x", function(a) {
-                var r = t(a.num);
-                return r > i ? e + r - 10 : e + r + 10
-            }).attr("y", this.barHeight / 2).attr("dy", "0.25em").attr("class", function(e) {
-                return t(e.num) > i ? "lk-white" : "lk-blue"
-            }).attr("style", function(e) {
-                return t(e.num) > i ? "font-size:1em;text-anchor:end" : "font-size:1em;text-anchor:start"
-            }).text(function(t) {
-                return n.call(a.price, t.num)
-            }), h.append("text").attr("x", this.spaceX - 5).attr("y", this.barHeight / 2).attr("dy", "0.25em").attr("style", "font-size:0.8em;text-anchor:end").attr("class", "brand-face lk-white").text(function(t) {
-                return t.date.substr(0, 4)
-            })
-        }, this.render = function() {
+
+            h.append("rect")
+                .attr("x", this.spaceX)
+                .attr("width", this.width - this.spaceX)
+                .attr("height", this.barHeight - 2)
+                .attr("class", function(t, e)
+                {
+                    return e % 7 == 0 || e % 7 == 1 ? "bar--background weekend" : "bar--background weekday"
+                }), 
+
+            h.append("rect")
+                .attr("x", this.spaceX)
+                .attr("class", "lk-turq")
+                .attr("width", function(e)
+                {
+                    return t(e.num)
+                })
+                .attr("height", this.barHeight - (2 + 2 * this.space))
+                .attr("y", this.space), 
+
+            h.append("text")
+                .attr("x", function(a) {
+                    var r = t(a.num);
+                    return r > i ? e + r - 10 : e + r + 10
+                })
+                .attr("y", this.barHeight / 2)
+                .attr("dy", "0.25em")
+                .attr("class", function(e) {
+                    return t(e.num) > i ? "lk-white" : "lk-blue"
+                })
+                .attr("style", function(e) {
+                    return t(e.num) > i ? "font-size:1em;text-anchor:end" : "font-size:1em;text-anchor:start"
+                })
+                .text(function(t) {
+                    return n.call(a.price, t.num)
+                }), 
+
+            h.append("text")
+                .attr("x", this.spaceX - 5)
+                .attr("y", this.barHeight / 2)
+                .attr("dy", "0.25em")
+                .attr("style", "font-size:0.8em;text-anchor:end")
+                .attr("class", "brand-face lk-blue").text(function(t) {
+                    return t.date.substr(0, 4)
+                })
+        }, 
+
+        this.render = function()
+        {
             return void 0 === this.price ? void a.error("Charles Babbage", "With dayBarChart you need to setType before calling render.") : (this.createScales(), void this.plotGraph())
         }
     }
--- src/js/actuate/monthUsageGraph.js
+++ src/js/actuate/monthUsageGraph.js
@@ -6,7 +6,15 @@ function(t, a, s)
     // e = actuate
     s.monthUsageGraph = function(i, h, e)
     {
-        this.graph = t(i).svgGraph(h, e), this.padding = 30, this.width = h - this.padding, this.height = e - 20, this.data = [], this.max = 0, this.type = "", this.setMax = function(t)
+        this.graph = t(i).svgGraph(h, e), 
+        this.padding = 6, 
+        this.width = h - (this.padding*2), 
+        this.height = e - 24, 
+        this.data = [], 
+        this.max = 0, 
+        this.type = "", 
+
+        this.setMax = function(t)
         {
             this.max = t
         }, 
@@ -15,13 +23,18 @@ function(t, a, s)
         {
             this.type = t;            
             this.suffix = ("water" == t) ? "L" : "kWh";
-            this.padding = ("electricity" == t) ? 40;
-            // "electricity" == t && (this.padding = 40)
+            // this.padding = ("electricity" == t) ? 40;
+            // "electricity" == t && (this.padding = 40);
         }, 
 
         this.renderBackground = function()
         {
-            this.graph.append("rect").attr("class", "graph--background").attr("x", this.padding).attr("y", 0).attr("width", this.width).attr("height", this.height)
+            this.graph.append("rect")
+                .attr("class", "graph--background")
+                .attr("x", this.padding)
+                .attr("y", 0)
+                .attr("width", this.width)
+                .attr("height", this.height)
         }, 
 
         this.createScales = function()
@@ -34,42 +47,116 @@ function(t, a, s)
             var i = this.scaleX,
                 h = this.scaleY,
                 e = 0,
-                r = a.svg.area().x(function(t, a)
-                {
-                    return i(a)
-                }).y0(this.height).y1(function(t)
-                {
-                    return e += t.num, h(e)
-                }).interpolate("line");
-            this.graph.append("path").datum(t).attr("class", s).attr("transform", "translate(" + this.padding + ",0)").attr("d", r)
+                r = a.svg.area().x(function(t, a) {
+                        return i(a)
+                    })
+                    .y0(this.height)
+                    .y1(function(t) {
+                        return e += t.num, h(e)
+                    })
+                    .interpolate("line");
+
+            this.graph.append("path").datum(t)
+                .attr("class", s)
+                .attr("transform", "translate(" + this.padding + ",0)")
+                .attr("d", r);
         }, 
 
         this.addXAxis = function()
         {
-            var t = a.svg.axis().scale(this.scaleX).orient("bottom").tickSize(-this.height).ticks(32);
-            this.graph.append("g").attr("class", "x axis").attr("transform", "translate(" + this.padding + "," + this.height + ")").call(t);
+            var t = a.svg.axis()
+                .scale(this.scaleX)
+                .orient("bottom")
+                .tickSize(-this.height)
+                .ticks(32);
+
+            this.graph.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(" + this.padding + "," + this.height + ")").call(t);
+            
             var s = this.data.this.length - 1,
-                i = this.data.this[s].date.split(" ");
-            this.graph.append("text").attr("class", "citu-yellow brand-face").attr("style", "text-anchor:start;font-size:12px").attr("x", this.scaleX(s) + this.padding).attr("y", this.height + 16).text(i[0] + " " + i[1]), this.graph.append("rect").attr("class", "citu-yellow").attr("width", "1").attr("height", "8").attr("x", this.scaleX(s) - 1 + this.padding).attr("y", this.height - 2), this.addLineLabel(this.data.sums.last, "lk-white"), this.addLineLabel(this.data.sums.this, "citu-yellow"), this.addLineLabel(this.data.sums.average, "lk-beige")
+                i = this.data.this[s].date.split(" "); // today's date     
+            
+            /*
+                put today's date under the graph where today's date is.
+            */        
+            this.graph.append("text")
+                .attr("class", "brand-face today")
+                // .attr("style", "text-anchor:start;font-size:12px")
+                .attr("x", this.scaleX(s) + 2 + this.padding)
+                .attr("y", this.height + 20)
+                .text(i[0] + " " + i[1]), 
+
+            lineheight = this.scaleY(this.data.sums.this),
+                  
+            /*
+                draw the little line which goes down to today's date.
+            */
+            this.graph.append("rect")
+                .attr("class", "today")
+                .attr("width", "1")
+                // .attr("height", "8")
+                .attr("height", this.height + 10 - lineheight)
+                .attr("x", this.scaleX(s) - 1 + this.padding)
+                .attr("y", lineheight),
+
+            this.addLineLabel(this.data.sums.last, "label label-last"), 
+            this.addLineLabel(this.data.sums.this, "label label-this"),
+            this.addLineLabel(this.data.sums.average, "label label-average", true)
         }, 
 
-        this.addLineLabel = function(t, a)
+        this.addLineLabel = function(t, a, b)
         {
-            this.graph.append("text").attr("class", a + " brand-face").attr("style", "text-anchor:end;font-size:11px").attr("x", this.padding - 2).attr("y", this.scaleY(t) + 10).text(Math.round(t) + "" + this.suffix)
+            // b = typeof a !== 'undefined' ? b : false;
+            // var extra = (b) ? 40 : 0 ;
+
+            this.graph.append("text")
+                .attr("class", a + " brand-face")
+                .attr("style", "text-anchor:start;")
+                .attr("x", this.padding + 2)
+                .attr("y", this.scaleY(t) + 16).text(Math.round(t) + "" + this.suffix)
         }, 
 
         this.addLine = function(t)
         {
             var a = this.scaleY(t),
-                s = this.graph.append("g").attr("transform", "translate(" + this.padding + ",0)");
-            return s.append("line").attr("x1", -10).attr("x2", this.width).attr("y1", a).attr("y2", a), s
+                s = this.graph.append("g")
+                    .attr("transform", "translate(" + this.padding + ",0)");
+            
+            return s.append("line")
+                .attr("x1", 0)
+                .attr("x2", this.width)
+                .attr("y1", a)
+                .attr("y2", a), s
         }, 
 
         this.render = function(t)
         {
-            this.data = t, void 0 === this.suffix && s.error("William Bald", "monthUsageGraph needs you to set a type using setType.");
+            this.data = t, 
+            void 0 === this.suffix && s.error("William Bald", "monthUsageGraph needs you to set a type using setType.");
+            
             var i = a.max([t.sums.this, t.sums.last, t.sums.average]);
-            this.setMax(1.1 * i), this.renderBackground(), this.createScales(), this.plotGraph(t.last, "graph--area--prev"), this.addLine(t.sums.last, "last month").attr("class", "graph graph--previous"), this.plotGraph(t.this, "graph--area"), this.addLine(t.sums.this, "so far").attr("class", "graph graph--sofar"), this.addLine(t.sums.average, "average").attr("class", "graph graph--average").attr("stroke-dasharray", "2,2"), this.addXAxis()
+
+            this.setMax(1.1 * i), 
+            
+            // this.renderBackground(), 
+
+            this.createScales(), 
+            this.plotGraph(t.last, "graph--area--prev"), 
+
+            this.addLine(t.sums.last, "last month")
+                .attr("class", "graph graph--previous"), 
+
+            this.plotGraph(t.this, "graph--area"), 
+
+            this.addLine(t.sums.this, "so far")
+                .attr("class", "graph graph--sofar"), 
+
+            this.addLine(t.sums.average, "average")
+                .attr("class", "graph graph--average")
+                .attr("stroke-dasharray", "2,2"), 
+
+            this.addXAxis()
         }
     }
 }(jQuery, d3, actuate);
