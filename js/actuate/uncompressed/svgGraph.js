749c356da1731c5f5810d2f658770f59a3079e11
Tom Morgan <morganesque@gmail.com>
2014-03-18 10:39:17 +0000

    first commit of the styleguide files.

---
+++ js/actuate/uncompressed/svgGraph.js
@@ -0,0 +1,1 @@
+!function(t,e){t.fn.svgGraph=function(n,r){var a=t(this)[0],i=e.select(a).attr("viewBox","0 0 "+n+" "+r);return t(a.parentNode).attr("style","padding-top:"+100*r/n+"%"),i}}(jQuery,d3);
